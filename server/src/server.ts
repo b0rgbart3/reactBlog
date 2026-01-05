9ca71d7f8247e884cfea031d0701f5f0cf53b547
Bart Dority <b0rgbart3@gmail.com>
2026-01-05 10:36:04 -0800

    server any UI file

--- server/src/server.ts
+++ server/src/server.ts
@@ -601,12 +601,16 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Default route to public/index.html if exists
-app.get("/index.html", (req: any, res: any) => {
-  const indexPath = path.join(__dirname, "public", "index.html");
+// app.get("/index.html", (req: any, res: any) => {
+//   const indexPath = path.join(__dirname, "public", "index.html");
 
-  res.sendFile(indexPath);
+//   res.sendFile(indexPath);
 
+// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
+
 
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
